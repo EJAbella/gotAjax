a049d521d15013462c9bc2417c5664eb83eae355
Elijah <elijah.abella@aol.com>
2018-07-11 09:52:17 -0700

    Added link functionality and base style

--- script.js
+++ script.js
@@ -1,44 +1,68 @@
-const charField = document.querySelector('input[name=charField]')
-const charSubmit = document.querySelector('button[name=charSubmit]')
+const field = document.querySelector('input[name=field]')
+const search = document.querySelector('button[name=search]')
+const type = document.querySelector('select[name=type]')
 const charName = document.querySelector('#charName')
 const charTitle = document.querySelector('#charTitle')
 const charSpouse = document.querySelector('#charSpouse')
-const houseField = document.querySelector('input[name=houseField]')
-const houseSubmit = document.querySelector('button[name=houseSubmit]')
 const houseName = document.querySelector('#houseName')
 const houseRegion = document.querySelector('#houseRegion')
 const houseLeader = document.querySelector('#houseLeader')
 const houseHeir = document.querySelector('#houseHeir')
 
-charSubmit.addEventListener('click', e => {
-  e.preventDefault()
-  axios.get(`https://anapioficeandfire.com/api/characters/${charField.value}`)
+let leaderHolder = ''
+let heirHolder = ''
+let spouseHolder = ''
+
+const updateChar = (link) => {
+  event.preventDefault()
+  axios.get(link)
     .then(response => {
       const data = response.data
-      charName.textContent = `Name: ${data.name}`
-      charTitle.textContent = `Titles: ${data.titles}`
-
+      charName.innerHTML = `Name: ${data.name}`
+      charTitle.innerHTML = `Titles: ${data.titles}`
+      spouseHolder = data.spouse
       axios.get(data.spouse)
         .then(response2 => {
-          charSpouse.innerHTML = `Spouse: <a href=''>${response2.data.name}<a>`
+          charSpouse.innerHTML = `Spouse: <a href='' onclick='updateChar(spouseHolder)'>${response2.data.name}<a>`
         })
     })
-})
 
-houseSubmit.addEventListener('click', e => {
-  e.preventDefault()
-  axios.get(`https://anapioficeandfire.com/api/houses/${houseField.value}`)
+}
+
+const charLoad = () => {
+  axios.get(`https://anapioficeandfire.com/api/characters/${field.value}`)
     .then(response => {
       const data = response.data
-      houseName.textContent = `House Name: ${data.name}`
-      houseRegion.textContent = `Region: ${data.region}`
-      axios.get(data.currentLord)
+      charName.innerHTML = `Name: ${data.name}`
+      charTitle.innerHTML = `Titles: ${data.titles}`
+      spouseHolder = data.spouse
+      axios.get(data.spouse)
         .then(response2 => {
-          houseLeader.textContent = `Current Leader: ${response2.data.name}`
-        })
-      axios.get(data.heir)
-        .then(response2 => {
-          houseHeir.textContent = `Heir: ${response2.data.name}`
+          charSpouse.innerHTML = `Spouse: <a href='' onclick='updateChar(spouseHolder)'>${response2.data.name}<a>`
         })
     })
+}
+
+const houseLoad = () => {
+  axios.get(`https://anapioficeandfire.com/api/houses/${field.value}`)
+    .then(response => {
+      const data = response.data
+      houseName.innerHTML = `House Name: ${data.name}`
+      houseRegion.innerHTML = `Region: ${data.region}`
+      leaderHolder = data.currentLord
+      axios.get(data.currentLord)
+        .then(response2 => {
+          houseLeader.innerHTML = `Current Leader: <a href='' onclick='updateChar(leaderHolder)'>${response2.data.name}</a>`
+        })
+      heirHolder = data.heir
+      axios.get(data.heir)
+        .then(response2 => {
+          houseHeir.innerHTML = `Heir: <a href='' onclick='updateChar(heirHolder)'>${response2.data.name}</a>`
+        })
+    })
+}
+
+search.addEventListener('click', e => {
+  e.preventDefault()
+  type.value === 'house' ? houseLoad() : charLoad()
 })
